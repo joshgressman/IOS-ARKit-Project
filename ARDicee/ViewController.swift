c883cbf1ee5dd7bfed65b6b461abfbcb55317846
joshgressman <joshgressman@gmail.com>
2018-04-23 18:58:28 -0500

    added cube object and new scene

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -20,12 +20,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
       
+        let cube = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let material = SCNMaterial()
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        material.diffuse.contents = UIColor.red
+        
+        cube.materials = [material]
+        
+        let node = SCNNode()
+        
+        node.position = SCNVector3(x: 0, y: 0.1, z: -0.5)
+        
+        node.geometry = cube
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        
+        sceneView.autoenablesDefaultLighting = true
+        
+//        // Create a new scene
+//        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+//
+//        // Set the scene to the view
+//        sceneView.scene = scene
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -34,6 +52,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
+        print("world tracking is \(ARWorldTrackingConfiguration.isSupported)")
         // Run the view's session
         sceneView.session.run(configuration)
     }

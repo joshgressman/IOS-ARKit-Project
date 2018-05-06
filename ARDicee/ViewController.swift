6fd85f736b1124b0371851f34e8c7e8574356d48
joshgressman <joshgressman@gmail.com>
2018-05-06 09:56:31 -0500

    added place dice on plane

--- ARDicee/ViewController.swift
+++ ARDicee/ViewController.swift
@@ -41,19 +41,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 //        sceneView.scene.rootNode.addChildNode(node)
         
         sceneView.autoenablesDefaultLighting = true
-        
-//        // Create a new scene
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-//
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-//
-//        diceNode.position = SCNVector3(x: 0, y: 0, z: -0.1)
-//
-//        sceneView.scene.rootNode.addChildNode(diceNode)
-//
-//        }
-    
-       
+
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -84,10 +72,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             let results = sceneView.hitTest(touchlocation, types: .existingPlaneUsingExtent)
             
-            if !results.isEmpty {
-                print("touched the plane")
-            } else {
-                print("touched outside the plane")
+                if let hitResult = results.first {
+                    
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                    
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    
+                diceNode.position = SCNVector3(x: hitResult.worldTransform.columns.3.x, y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius, z: hitResult.worldTransform.columns.3.z)
+                    
+                sceneView.scene.rootNode.addChildNode(diceNode)
+                    
+                }
             }
         }
     }
